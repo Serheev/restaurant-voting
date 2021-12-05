a0e9716c52a9a896bc2f308532e605a50afe22d7
Serheev <yserheev@gmail.com>
2021-12-05 19:20:39 +0200

    Refactor model query

--- src/main/java/com/serheev/restaurant/RestaurantVotingApplication.java
+++ src/main/java/com/serheev/restaurant/RestaurantVotingApplication.java
@@ -1,15 +1,23 @@
 package com.serheev.restaurant;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.serheev.restaurant.repository.UserRepository;
 
 @SpringBootApplication
 @AllArgsConstructor
-public class RestaurantVotingApplication {
+public class RestaurantVotingApplication implements ApplicationRunner {
+	private final UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantVotingApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) {
+		System.out.println(userRepository.findByLastNameContainingIgnoreCase("last"));
+	}
 }
